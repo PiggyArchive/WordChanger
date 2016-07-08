3fe56f378b46241d164f166e836f4f07c9830ff7
MCPEPIG <mcpepig123@gmail.com>
2016-07-07 19:46:05 -0700

    :3

--- src/WordChanger/Main.php
+++ src/WordChanger/Main.php
@@ -7,7 +7,7 @@ use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener{
     public function onEnable(){
-        $this->words = new Config($this->getDataFolder() . "words.yml", Config::YAML);
+        $this->words = new Config($this->getDataFolder() . "words.yml", Config::YAML, ["op" => "poo"]);
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
         $this->getLogger()->info("§aEnabled.");
     }
