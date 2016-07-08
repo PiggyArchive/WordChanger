650f501361b2670d3dc551541e9dadf13b5c34e5
MCPEPIG <mcpepig123@gmail.com>
2016-07-07 19:48:12 -0700

    Fix

--- src/WordChanger/Main.php
+++ src/WordChanger/Main.php
@@ -5,7 +5,7 @@ namespace WordChanger;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 
-class Main extends PluginBase implements Listener{
+class Main extends PluginBase{
     public function onEnable(){
         $this->words = new Config($this->getDataFolder() . "words.yml", Config::YAML, ["op" => "poo"]);
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
