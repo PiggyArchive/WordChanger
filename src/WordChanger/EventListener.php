560efa8825a4dd3055014de296df2745dc856870
Mr. Piggy <mcpepig123@gmail.com>
2016-07-12 15:39:45 -0700

    Fix formatting

--- src/WordChanger/EventListener.php
+++ src/WordChanger/EventListener.php
@@ -6,19 +6,19 @@ use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\Listener;
 
 class EventListener implements Listener{
-	public function __construct($plugin){
-		$this->plugin = $plugin;
-	}
+    public function __construct($plugin){
+        $this->plugin = $plugin;
+    }
 	
-	public function onPlayerChat(PlayerChatEvent $event){
+    public function onPlayerChat(PlayerChatEvent $event){
         $player = $event->getPlayer();
         $message = $event->getMessage();
-    	foreach($this->plugin->words->getAll() as $word => $replacement){
-      		if(!$player->hasPermission("wordchanger.override")){
-        		$message = str_ireplace($word, $replacement, $message);
-        		$event->setMessage($message);     
-      		}
-    	}
+        foreach($this->plugin->words->getAll() as $word => $replacement){
+            if(!$player->hasPermission("wordchanger.override")){
+                $message = str_ireplace($word, $replacement, $message);
+                $event->setMessage($message);     
+            }
+        }
     }
 
 }
